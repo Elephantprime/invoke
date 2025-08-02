4425d439dbded4f4869dd1c6105eef4efd93b073
Elephantprime <jkksr5@gmail.com>
2025-08-02 08:53:47 -0400

    Update invoke.js

--- invoke.js
+++ invoke.js
@@ -1,1 +1,18 @@
+/* jshint esversion: 8 */
+/* global CONFIG, loadConfig, fetchGPT, speak */
 
+async function handleInput() {
+  if (!CONFIG.OPENAI_KEY) await loadConfig();
+
+  const input = document.getElementById("userInput").value;
+  const output = document.getElementById("output");
+
+  output.innerHTML += `<div><b>You:</b> ${input}</div>`;
+
+  const response = await fetchGPT(input);
+
+  output.innerHTML += `<div><b>INVOKE:</b> ${response}</div>`;
+  speak(response);
+
+  document.getElementById("userInput").value = "";
+}
