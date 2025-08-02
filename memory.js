266b86e550948c461f083f79f1e1de86e3f4416c
Elephantprime <jkksr5@gmail.com>
2025-08-02 10:08:33 -0400

    Update memory.js

--- memory.js
+++ memory.js
@@ -1,1 +1,10 @@
+const memory = [];
 
+function addHistory(input, response) {
+  memory.push({ input, response });
+  if (memory.length > 50) memory.shift();
+}
+
+function getHistory() {
+  return memory;
+}
